c7adb2129fdd1db7eb1ea32e40a3c93f6f456a94
legorockband <tarenner@calpoly.edu>
2024-11-20 20:12:30 -0800

    Works for 1 loop but stops after entering second int

--- square_plus1.c
+++ square_plus1.c
@@ -11,7 +11,6 @@ int main(){
     int pipe_p_c1[2], pipe_c1_c2[2], pipe_c2_p[2];
     pid_t child1, child2;
    
-    
     // Create pipe from parent to child 1 
     if(pipe(pipe_p_c1) == - 1){
         perror("Parent to Child 1 pipe failed");
@@ -36,29 +35,30 @@ int main(){
     }
     // Child 1 Process
     if(child1 == 0){
-        // Child only wants to read from pipe, closes write side
-        close(pipe_p_c1[1]);    
+        close(pipe_p_c1[1]);    // Close write for parent to child1
+        close(pipe_c1_c2[0]);   // Close Read for child1 to 2
 
-        // Parent sends child val 
-        int buffer[10];        
-        if(read(pipe_p_c1[0], buffer, sizeof(int)) == -1){
+        //Close unused pips
+        close(pipe_c2_p[0]);
+        close(pipe_c2_p[1]);
+
+        int buffer;        
+        // Read the value from the parent into child1 
+        if(read(pipe_p_c1[0], &buffer, sizeof(int)) == -1){
             perror("Child 1: Read failed");
             exit(EXIT_FAILURE);
         }
-        // Close the read side
-        close(pipe_p_c1[0]);       
+        // Square the val from the parent
+        buffer *= buffer;
         
-        // Square the val
-        buffer[0] *= buffer[0];
-        
-        // Write: child1 to child2
-        close(pipe_c1_c2[0]);
-        if(write(pipe_c1_c2[0], buffer, sizeof(int)) == -1){
+        // Write the value to child2
+        if(write(pipe_c1_c2[1], &buffer, sizeof(int)) == -1){
             perror("Child 1: Write Failed");
             exit(EXIT_FAILURE);
         }
+        close(pipe_p_c1[0]);       
         close(pipe_c1_c2[1]);
-        exit(9);
+        exit(0);
     }
     // Back in Parent
     else{
@@ -70,76 +70,80 @@ int main(){
         // Child 2 process
         if(child2 == 0){
             
-            // Child 2 only wants to read from pipe, closes write side
-            close(pipe_c1_c2[1]);    
+            close(pipe_c1_c2[1]);   // Close write from child1 to 2    
+            close(pipe_c2_p[0]);    // Close read from child2 to parent
+
+            //Not used pipes
+            close(pipe_p_c1[0]);
+            close(pipe_p_c1[1]);
 
             // Child 1 sends child 2 val 
-            int buffer[10];        
-            if(read(pipe_c1_c2[0], buffer, sizeof(int)) == -1){
+            int buffer;        
+            if(read(pipe_c1_c2[0], &buffer, sizeof(int)) == -1){
                 perror("Child 2: Read failed");
                 exit(EXIT_FAILURE);
             }
-            // Close the read side
-            close(pipe_c1_c2[0]);
-            
             // Add 1 to buffer
-            buffer[0] += 1;
+            buffer += 1;
         
-            // Write: child2 to parent
-            close(pipe_c2_p[0]);
-            if(write(pipe_c2_p[0], buffer, sizeof(int)) == -1){
+            // Write value from child2 to parent
+            if(write(pipe_c2_p[1], &buffer, sizeof(int)) == -1){
                 perror("Child 2: Write Failed");
                 exit(EXIT_FAILURE);
             }
+            close(pipe_c1_c2[0]);
             close(pipe_c2_p[1]);      
-            exit(10);
+            exit(0);
         }
     
         // Back in Parent
         else{
-            // Close the read end of parent to child1 pipe
-            close(pipe_p_c1[0]);
-            int val[10];
-            printf("Enter Integer: ");
-            scanf("%d", &val[0]);
+            close(pipe_p_c1[0]);    // Close the read end of parent to child1 pipe 
+            close(pipe_c2_p[1]);    // Close write end of child2 to parent
             
-            // Write val to the child 1    
-            if(write(pipe_p_c1[1], val, sizeof(int)) == -1){
-                perror("Write Failed");
-                exit(EXIT_FAILURE);
+            //Unused Pipes
+            close(pipe_c1_c2[0]);
+            close(pipe_c1_c2[1]);
+
+            int val;
+            while(1){
+                printf("Enter an Integer (CRTL+D to exit): ");
+                if(scanf("%d", &val) != 1){
+                    printf("hello\n");
+                    break;
+                }
+                // Write val to the child 1    
+                if(write(pipe_p_c1[1], &val, sizeof(int)) == -1){
+                    perror("Write Failed");
+                    exit(EXIT_FAILURE);
+                }
+            
+                // Read the final value 
+                if(read(pipe_c2_p[0], &val, sizeof(int)) == -1){
+                    perror("Child 2: Read failed");
+                    exit(EXIT_FAILURE);
+                }
+            
+                printf("Square + 1: %d\n", val);                      
             }
+            
+            // Close the read side
             close(pipe_p_c1[1]);
-   
+            close(pipe_c2_p[0]);
+       
             // Wait for child 1 
             if(waitpid(child1, &status1, 0) == -1){
                 perror("Child 1: Wait Failed");
                 exit(EXIT_FAILURE);
             }
-            printf("First child exited with status %d\n", WEXITSTATUS(status1));
             // Wait for child 2 to finish to get new value
             if(waitpid(child2, &status2, 0) == -1){
                 perror("Child 2: Wait Failed");
                 exit(EXIT_FAILURE);
             }
-            printf("Second child exited with status %d\n", WEXITSTATUS(status2));
-            
-            // Close write end 
-            close(pipe_c2_p[1]);
-            
-            int final_val[10];        
-            if(read(pipe_c2_p[0], final_val, sizeof(int)) == -1){
-                perror("Child 2: Read failed");
-                exit(EXIT_FAILURE);
-            }
-            // Close the read side
-            close(pipe_c2_p[0]);
-            
-            printf("Square + 1: %d\n", final_val[0]);                      
-
         }
     
     }
-
 
     return 1;
 }
